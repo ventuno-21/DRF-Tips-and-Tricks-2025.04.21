68b58c03653b682ab1c7e2be02bb1215a47916e8
ventuno-21 <yasna.pakdel@outlook.com>
2025-04-22 22:45:30 -0700

    14 Installation of drf-spactacular

--- drf_course/settings.py
+++ drf_course/settings.py
@@ -31,6 +31,7 @@ INSTALLED_APPS = [
     # Third party apps
     "rest_framework",
     "silk",
+    "drf_spectacular",
 ]
 
 MIDDLEWARE = [
@@ -122,5 +123,14 @@ REST_FRAMEWORK = {
     "DEFAULT_AUTHENTICATION_CLASSES": [
         "rest_framework_simplejwt.authentication.JWTAuthentication",
         "rest_framework.authentication.SessionAuthentication",
-    ]
+    ],
+    "DEFAULT_SCHEMA_CLASS": "drf_spectacular.openapi.AutoSchema",
+}
+
+SPECTACULAR_SETTINGS = {
+    "TITLE": "Ventuno E-commerce",
+    "DESCRIPTION": "My project description",
+    "VERSION": "1.0.0",
+    "SERVE_INCLUDE_SCHEMA": False,
+    # OTHER SETTINGS
 }
